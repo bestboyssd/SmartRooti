750ad368eb0d4642d9cb8e0d1524050447c1586c
bestboyssd <164778371+bestboyssd@users.noreply.github.com>
2026-03-28 10:46:51 +0530

    Create user account profile page

---
+++ src/pages/Account.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Account = () => {
+    return (
+        <div>
+            <h1>User Account Profile</h1>
+            <p>This is the user account profile page.</p>
+        </div>
+    );
+};
+
+export default Account;
