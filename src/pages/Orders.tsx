4fef01d513953d70343cb7dcb6515dd4410c3d06
bestboyssd <164778371+bestboyssd@users.noreply.github.com>
2026-03-28 10:46:55 +0530

    Create Orders history page

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h1>Orders History</h1>
+            <p>This will contain the user's order history.</p>
+        </div>
+    );
+};
+
+export default Orders;
